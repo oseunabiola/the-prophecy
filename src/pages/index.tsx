4b055d8e62ea05f261977bdc327d2a33a12482ea
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2025-02-23 18:53:30 +0100

    navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,51 @@
+import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-   return (
-      <div>
-         <Nav />
-      </div>
-   );
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export { Home };
 
 const Nav = () => {
-   return (
-      <div className="m-4 bg-red-400">
-         <a href="/" className="">
+  return (
+    <header>
+      <PageContainer>
+        <div className="m-4 flex items-center justify-between bg-primary px-5 py-5">
+          <Link to="/" className="text-xl font-semibold">
             That all may Prophesy
-         </a>
-         <nav>
-            <ul>
-               <li>
-                  <Link to="/">Home</Link>
-               </li>
-               <li>
-                  <Link to="/">Home2</Link>
-               </li>
+          </Link>
+          <nav>
+            <ul className="flex gap-x-6">
+              <li>
+                <Link to="/">The Book</Link>
+              </li>
+              <li>
+                <Link to="/">Enroll for Mentorship</Link>
+              </li>
+              <li>
+                <Link to="/">Join Community</Link>
+              </li>
+              <li>
+                <Link to="/">The Author</Link>
+              </li>
+              <div className="cta |">
+                <Link to="" className="rounded-md bg-black px-8 py-3 text-alt">
+                  Order Now
+                </Link>
+              </div>
             </ul>
-         </nav>
-      </div>
-   );
+          </nav>
+        </div>
+      </PageContainer>
+    </header>
+  );
+};
+
+const PageContainer = ({ children }: { children: ReactNode }) => {
+  return <div className="mx-auto w-10/12 xl:w-[83%]">{children}</div>;
 };
