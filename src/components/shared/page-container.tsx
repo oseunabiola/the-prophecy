54d478bf90ca9f0f750aceb943fd3bd2a20ebdf8
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2025-02-25 02:04:03 +0100

    mobile wip

--- src/components/shared/page-container.tsx
+++ src/components/shared/page-container.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const PageContainer = ({ children }: { children: ReactNode }) => {
-    return <div className="mx-auto w-11/12 xl:w-[83%]">{children}</div>;
+    return <div className="mx-auto w-10/12">{children}</div>;
 };
 
 export { PageContainer };
